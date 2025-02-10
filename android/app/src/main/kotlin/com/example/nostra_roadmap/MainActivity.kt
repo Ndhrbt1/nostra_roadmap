dbb8a759b50205b226db3d7cfd38f092c3d4c5b7
Ndhrbt1 <indahra04always@gmail.com>
2025-02-10 21:02:17 +0700

    feat: counter_statefull

---
+++ android/app/src/main/kotlin/com/example/nostra_roadmap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nostra_roadmap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:nostra_roadmap/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
