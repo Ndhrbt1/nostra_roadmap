494d2656187d61667809ba2f2240e3b08864eefd
Ndhrbt1 <indahroad04@gmail.com>
2025-06-03 15:35:03 +0700

    chore: rename file

--- lib/app/navigation/_index.dart
+++ lib/app/navigation/_index.dart
@@ -1,5 +1,5 @@
 import 'package:navigation_builder/navigation_builder.dart';
-import 'package:nostra_roadmap/ui_screens/b.product/a.product_list/_index.dart';
+import 'package:nostra_roadmap/ui_screens/b.product_y/a.product_list/_index.dart';
 
 import '../../ui_screens/_index.dart';
 
--- lib/ui_screens/_index.dart
+++ lib/ui_screens/_index.dart
@@ -6,8 +6,8 @@ export 'x.sample/b.sample_dua/_index.dart';
 
 export 'x.sample/c.sample_tiga/_index.dart';
 
-export 'b.product_x/_index.dart';
+export 'b.product_z/_index.dart';
 
-export 'b.product/b.product_detail/_index.dart';
-export 'b.product/c.product_add/_index.dart';
-export 'b.product/d.product_edit/_index.dart';
+export 'b.product_y/b.product_detail/_index.dart';
+export 'b.product_y/c.product_add/_index.dart';
+export 'b.product_y/d.product_edit/_index.dart';
